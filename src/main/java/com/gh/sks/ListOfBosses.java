807ab3bf5bbd65a8b1d8ee95f0cf1298146ee9f8
Cerbean <106828590+Cerbean@users.noreply.github.com>
2023-01-13 17:54:26 -0500

    Updated RLI and added List of NPCs that will be used to check the drops of bosses.

---
+++ src/main/java/com/gh/sks/ListOfBosses.java
@@ -0,0 +1,16 @@
+package com.gh.sks;
+
+import java.util.ArrayList;
+
+public class ListOfBosses
+{
+    public static ArrayList<String> listOfBosses = new ArrayList<>();
+
+    public static void setListOfBosses()
+    {
+        listOfBosses.add("Man");
+        listOfBosses.add("Spider");
+    }
+
+
+}
