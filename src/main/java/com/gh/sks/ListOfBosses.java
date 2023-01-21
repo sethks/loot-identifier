2af3d771e5001a3cbc0ee204922f949d32ed2570
Cerbean <106828590+Cerbean@users.noreply.github.com>
2023-01-21 12:31:32 -0500

    pushing changes to master

--- src/main/java/com/gh/sks/ListOfBosses.java
+++ src/main/java/com/gh/sks/ListOfBosses.java
@@ -81,6 +81,7 @@ public class ListOfBosses
     public static void setAllMap()
     {
         mapOfAllItems.put("teset something", "id=232");
+        mapOfAllItems.put("anoter test", "sdfsdf");
     }
 
 
